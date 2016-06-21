f4a5cade981378b73787dfaf5c15440da4b122a7
Joshua Rivers <joshua.rivers@gmail.com>
2016-06-21 07:32:05 -0400

    normal mode part 1 complete

--- js/main.js
+++ js/main.js
@@ -5,11 +5,22 @@ var tickClock = function startTime() {
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
+  h = checkHour(h);
   m = checkTime(m);
   s = checkTime(s);
   clock.innerHTML = h + ":" + m + ":" + s;
   var t = setTimeout(startTime, 500);
 }
+
+function checkHour(i) {
+  if (i > 12) {
+    i = "0" + (i - 12)
+  } else if (i < 10) {
+    i = "0" + i
+  }
+
+  return i;
+};
 
 function checkTime(i) {
   if (i < 10) {
